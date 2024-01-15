b7c10cd562875ac994e5b363879e3625fb87deea
Seyoung Hong <maiorem00@gmail.com>
2024-01-15 21:53:15 +0900

    Initial commit

---
+++ src/main/java/com/art/api/ArtvuApplication.java
@@ -0,0 +1,13 @@
+package com.art.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtvuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtvuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/art/api/ArtvuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtvuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
