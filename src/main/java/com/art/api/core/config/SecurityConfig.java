98ec0f2b3747a3b4b4e4edd28d5bffe6876ad9c9
Seyoung Hong <maiorem00@gmail.com>
2024-01-16 23:34:02 +0900

    #1 - 프로젝트 기본 세팅

---
+++ src/main/java/com/art/api/core/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.art.api.core.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/healthCheck", "/images/**", "/view/join", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(withDefaults());
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/art/api/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.art.api.member.repository;
+
+import com.art.api.member.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+}
