7f3d6f16b31a521982932d503af04519a4ba8f06
Seyoung Hong <maiorem00@gmail.com>
2024-02-24 19:07:30 +0900

    setting:logback

--- src/main/java/com/art/api/HealthCheckController.java
+++ src/main/java/com/art/api/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/")
+    @GetMapping("/healthCheck")
     public String healthCheck() {
         return "health check!";
     }
