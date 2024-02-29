309fc516fd12634181bdd88fc4df7378eda41df8
Seyoung Hong <sehong@nuskin.com>
2024-02-29 13:13:16 +0900

    feat: #1 - ItemReader open-api list 적용

--- src/main/java/com/art/api/scheduler/application/openApiRecords/KopisArtListResponse.java
+++ src/main/java/com/art/api/scheduler/application/openApiRecords/KopisArtListResponse.java
@@ -1,34 +1,35 @@
 package com.art.api.scheduler.application.openApiRecords;
 
 import com.art.api.scheduler.domain.entity.KopisArtList;
+import lombok.Data;
 
-public record KopisArtListResponse(
-        String mt20id, //공연ID
-        String prfnm,//공연명
-        String prfpdfrom,//공연시작시간
-        String prfpdto,//공연종료시간
-        String fcltynm,//공연시설명
-        String poster,//포스터
-        String area,//지역
-        String genrenm,//장르
-        String prfstate,//공연상태
-        String openrun //오픈런여부
-) {
+import java.util.List;
 
-    public KopisArtList toEntity() {
-        return KopisArtList.builder()
-                .artId(mt20id)
-                .artNm(prfnm)
-                .artStrDt(prfpdfrom)
-                .artEndDt(prfpdto)
-                .artFacNm(fcltynm)
-                .posterImgUrl(poster)
-                .artAreaNm(area)
-                .genreNm(genrenm)
-                .status(prfstate)
-                .openrunYn(openrun)
-                .build();
+
+@Data
+public class KopisArtListResponse{
+
+    private Dbs db;
+
+    @Data
+    public static class Dbs {
+
+        private List<Db> db;
+
+        @Data
+        public static class Db {
+            private String mt20id; //공연ID
+            private String prfnm;//공연명
+            private String prfpdfrom;//공연시작시간
+            private String prfpdto;//공연종료시간
+            private String fcltynm;//공연시설명
+            private String poster;//포스터
+            private String area;//지역
+            private String genrenm;//장르
+            private String prfstate;//공연상태
+            private String openrun; //오픈런여부
+        }
+
     }
-
 
 }
