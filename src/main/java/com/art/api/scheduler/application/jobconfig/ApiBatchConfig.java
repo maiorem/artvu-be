da1ee5d891f20eb02bd51aa16679eb2fec10b44a
Seyoung Hong <sehong@nuskin.com>
2024-03-04 17:32:39 +0900

    feat: #1 - batch step1 (kopis list api)

--- src/main/java/com/art/api/scheduler/application/jobconfig/ApiBatchConfig.java
+++ src/main/java/com/art/api/scheduler/application/jobconfig/ApiBatchConfig.java
@@ -1,23 +1,25 @@
 package com.art.api.scheduler.application.jobconfig;
 
+import com.art.api.scheduler.application.jobconfig.list.ArtListItemProcessor;
+import com.art.api.scheduler.application.jobconfig.list.ArtListItemReader;
+import com.art.api.scheduler.application.jobconfig.list.ArtListItemWriter;
+import com.art.api.scheduler.application.openApiRecords.KopisArtListResponse;
 import com.art.api.scheduler.domain.entity.KopisArtList;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -25,27 +27,44 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class ApiBatchConfig {
 
-//    private final KopisArtListApiRepository listApiRepository;
-//    private final KopisArtDetailApiRepository detailApiRepository;
-//    private final KopisArtFacDetailApiRepository facDetailApiRepository;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final EntityManagerFactory entityManager;
 
-    // Art List Job
-//    @Bean
-//    public Job listReaderJob(JobRepository jobRepository, Step listReaderStep){
-//        return new JobBuilder("listReaderJob", jobRepository)
-//                .start(listReaderStep)
-//                .build();
-//    }
+    @Bean
+    public Job listReaderJob(){
+        return new JobBuilder("listReaderJob", jobRepository)
+                .start(listReaderStep())
+                .build();
+    }
 
+    // Step 1 (list)
+    @Bean
+    public Step listReaderStep() {
+        return new StepBuilder("listReaderStep", jobRepository)
+                .<KopisArtListResponse, List<KopisArtList>>chunk(2, transactionManager)
+                .reader(listReader())
+                .processor(listItemProcessor())
+                .writer(listArtListItemWriter())
+                .build();
+    }
 
+    @Bean
+    public ArtListItemReader listReader() {
+        return new ArtListItemReader();
+    }
 
-//    // Step 1 (list)
-//    @JobScope
-//    @Bean
-//    public Step listReaderStep(JobRepository jobRepository, ItemReader apiListReader, ItemProcessor apiListProcessor, ItemWriter apiListWriter) {
-//        return new StepBuilder("listReaderStep", jobRepository)
-//                .build();
-//    }
+    @Bean
+    public ArtListItemProcessor listItemProcessor() {
+        return new ArtListItemProcessor();
+    }
+
+    @Bean
+    public ArtListItemWriter<KopisArtList> listArtListItemWriter() {
+        JpaItemWriter<KopisArtList> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManager);
+        return new ArtListItemWriter<>(writer);
+    }
 
 
 }
--- src/main/java/com/art/api/scheduler/application/jobconfig/list/ArtListItemWriter.java
+++ src/main/java/com/art/api/scheduler/application/jobconfig/list/ArtListItemWriter.java
@@ -1,18 +1,29 @@
 package com.art.api.scheduler.application.jobconfig.list;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.database.JpaItemWriter;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ArtListItemWriter<T> extends JpaItemWriter<List<T>> {
 
     private final JpaItemWriter<T> jpaItemWriter;
 
+
     @Override
     public void write(Chunk<? extends List<T>> items) {
-        super.write(items);
+        Chunk<T> collect = new Chunk<T>();
+        for(List<T> list : items){
+            collect.addAll(list);
+        }
+
+        jpaItemWriter.write(collect);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        super.afterPropertiesSet();
     }
 }
