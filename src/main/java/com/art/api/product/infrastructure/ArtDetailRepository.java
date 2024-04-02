b7582a581062971c0669434e3418197ce166c296
Seyoung Hong <sehong@nuskin.com>
2024-04-02 14:32:44 +0900

    feat: #3 - 디스커버 API

--- src/main/java/com/art/api/product/infrastructure/ArtDetailRepository.java
+++ src/main/java/com/art/api/product/infrastructure/ArtDetailRepository.java
@@ -1,14 +1,14 @@
 package com.art.api.product.infrastructure;
 
-import com.art.api.product.domain.entity.ArtList;
+import com.art.api.product.domain.entity.ArtDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ArtListRepository extends JpaRepository<ArtList, String>, ArtListRepositoryCustum {
+public interface ArtDetailRepository extends JpaRepository<ArtDetail, Integer> {
 
-    Optional<ArtList> findByArtId(String artId);
+    Optional<ArtDetail> findByArtId(String artId);
 
 }
