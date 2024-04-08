5abe95dfa37d8918e25a9ca69dd78c5451816413
Seyoung Hong <sehong@nuskin.com>
2024-04-08 17:15:29 +0900

    feat: #4 - 공연 단건 및 시설 정보 추가

--- src/main/java/com/art/api/facility/application/ArtFacService.java
+++ src/main/java/com/art/api/facility/application/ArtFacService.java
@@ -1,26 +1,56 @@
 package com.art.api.facility.application;
 
+import com.art.api.facility.domain.dto.SeatDTO;
+import com.art.api.facility.domain.dto.TheaterDTO;
 import com.art.api.facility.domain.entity.ArtFacDetail;
 import com.art.api.facility.infrastructure.ArtFacRepository;
+import com.art.api.product.domain.entity.ArtImg;
+import com.art.api.product.domain.entity.ArtList;
+import com.art.api.product.domain.entity.ClsCode;
+import com.art.api.product.infrastructure.ArtImgRepository;
+import com.art.api.product.infrastructure.ArtListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ArtFacService {
 
-    private final ArtFacRepository artFacRepository;
 
-    public ArtFacDetail retrieveFacility(String facId) {
+    private final ArtListRepository artRepository;
+    private final ArtFacRepository artFacRepository;
+    private final ArtImgRepository imgRepository;
+
+    public TheaterDTO retrieveFacility(String facId) {
 
         Optional<ArtFacDetail> byFacId = artFacRepository.findByArtFacId(facId);
         if(byFacId.isEmpty()) {
 
         }
-        return byFacId.get();
+        return TheaterDTO.convertEntityToDto(byFacId.get());
     }
+
+    public SeatDTO regreiveTip(String artId) {
+        Optional<ArtList> art = artRepository.findByArtId(artId);
+        if(art.isEmpty()) {
+
+        }
+        Optional<List<ArtImg>> imgList = imgRepository.findAllByArtList(art.get());
+        if(imgList.isEmpty()) {
+
+        }
+        for (ArtImg artImg : imgList.get()) {
+            if(artImg.getClsCode().equals(ClsCode.SEAT)) {
+               return new SeatDTO(artImg.getImgUrl());
+            }
+        }
+        return null;
+
+    }
+
 
 
 }
--- src/main/java/com/art/api/product/infrastructure/ArtTimeRepository.java
+++ src/main/java/com/art/api/product/infrastructure/ArtTimeRepository.java
@@ -1,9 +1,13 @@
 package com.art.api.product.infrastructure;
 
+import com.art.api.product.domain.entity.ArtList;
 import com.art.api.product.domain.entity.ArtTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ArtTimeRepository extends JpaRepository<ArtTime, Integer> {
+    Optional<ArtTime> findByArtlist(ArtList artList);
 }
