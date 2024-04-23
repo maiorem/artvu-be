b0cec6ec490d09c89dd8e2dd4d3de25aa9f423be
Seyoung Hong <maiorem00@gmail.com>
2024-04-23 21:33:51 +0900

    feature: #4 - 장르 매핑 수정

--- src/main/java/com/art/api/product/infrastructure/ArtGenreMppgRepository.java
+++ src/main/java/com/art/api/product/infrastructure/ArtGenreMppgRepository.java
@@ -1,7 +1,6 @@
 package com.art.api.product.infrastructure;
 
 import com.art.api.product.domain.entity.ArtGenreMppg;
-import com.art.api.product.domain.entity.ArtGenreMppgId;
 import com.art.api.product.domain.entity.ArtList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ArtGenreMppgRepository extends JpaRepository<ArtGenreMppg, ArtGenreMppgId> {
+public interface ArtGenreMppgRepository extends JpaRepository<ArtGenreMppg, Integer> {
 
     Optional<List<ArtGenreMppg>> findAllByArtList(ArtList artList);
 
