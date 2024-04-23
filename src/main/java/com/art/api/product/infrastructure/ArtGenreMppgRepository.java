c52a74899bc09fe028fdc4a4f91977978b8401c7
Seyoung Hong <sehong@nuskin.com>
2024-04-23 13:16:44 +0900

    feat: #4 - 장르 매핑 방식 수정 / 회원탈퇴 카카오 unlink

--- src/main/java/com/art/api/product/infrastructure/ArtGenreMppgRepository.java
+++ src/main/java/com/art/api/product/infrastructure/ArtGenreMppgRepository.java
@@ -4,12 +4,14 @@ import com.art.api.product.domain.entity.ArtGenreMppg;
 import com.art.api.product.domain.entity.ArtGenreMppgId;
 import com.art.api.product.domain.entity.ArtList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ArtGenreMppgRepository extends JpaRepository<ArtGenreMppg, ArtGenreMppgId> {
 
-    Optional<List<ArtGenreMppg>> findAllByArtList(String artId);
+    Optional<List<ArtGenreMppg>> findAllByArtList(ArtList artList);
 
 }
