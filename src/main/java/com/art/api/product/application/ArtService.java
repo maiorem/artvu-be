503dbcd017138d33fb8d7201a2dcb7f3200038f9
Seyoung Hong <maiorem00@gmail.com>
2024-05-12 17:38:30 +0900

    feature: API 에러 응답 및 검색 결과 수정

--- src/main/java/com/art/api/product/application/ArtService.java
+++ src/main/java/com/art/api/product/application/ArtService.java
@@ -67,7 +67,7 @@ public class ArtService {
             list.add(dto);
         }
 
-        Page<ArtListDTO> result = new PageImpl<>(list, artList.getPageable(), artList.getSize());
+        Page<ArtListDTO> result = new PageImpl<>(list, artList.getPageable(), artList.getTotalElements());
         return result;
     }
 
@@ -132,8 +132,9 @@ public class ArtService {
         for (Theme theme : themeList) {
             List<ThemeDTO> list = new ArrayList<>();
             Optional<List<ThemeHist>> themeHistList = themeHistRepository.findByTheme(theme);
-            themeHistList.ifPresent(themeHists -> {
-                for (ThemeHist themeHist : themeHists) {
+            if (themeHistList.isPresent()) {
+
+                for (ThemeHist themeHist : themeHistList.get()) {
                     Optional<ArtList> art = artListRepository.findByArtId(themeHist.getArtList().getArtId());
                     if (art.isEmpty()) {
                         throw new ItemNotFoundException();
@@ -147,7 +148,9 @@ public class ArtService {
                     dto.setPosterImgUrl(posterUrl);
                     list.add(dto);
                 }
-            });
+            } else {
+                return null;
+            }
             ThemeListDTO newListDto = ThemeListDTO.convertEntityToDto(theme);
             newListDto.setContents(list);
             dtoList.add(newListDto);
